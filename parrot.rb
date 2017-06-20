2aed62e83b2678ebee7f3e66e740fb9b692293e2
Alice Liang <aliceliang011@gmail.com>
2017-06-20 22:45:35 +0000

    changed parrot

--- parrot.rb
+++ parrot.rb
@@ -4,23 +4,31 @@ def happy_parrot(thing)
 end
 
 # Boring Parrot - Write a method for a boring parrot that just returns whatever string you give him as an argument.
-
-
+def boring_parrot(string)
+    "#{string}"
+end
+puts boring_parrot("I don't want to talk to you")
 
 # Math Parrot - Create a method that accepts two numbers as arguments and adds them together! 
-
-
+def math_parrot(number1,number2)
+    number1+number2
+end
+puts math_parrot(20,210)
 
 # Friendly Parrot - This parrot greets people by returning their name and age (don't forget to pass that information in as arguments).
-
-
+def friendly_parrot(name,age)
+    "hello #{name}, you are #{age}"
+end
+puts friendly_parrot("alice", "17")
 
 # Favorites Parrot - Tell this parrot about your three favorite things and he will return "I love <that thing> too!" for each of them.
-
-
+def favorites_parrot(object1,object2,object3)
+    "I love #{object1},#{object2} and #{object3} too!"
+end
+puts favorites_parrot(food,vacation,photography)
 
 # Now try calling your methods below with any arguments of your choice and puts them to the screen. Like this:
-puts happy_parrot("waffles")
+
 # call your methods here
 
 
